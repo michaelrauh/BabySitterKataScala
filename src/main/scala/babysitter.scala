f3a85a3b7731c50327f7f20e9460d39535d8d4ae
Michael Rauh <michaelrauh@MichaelRauh.local>
2016-05-09 14:37:47 -0400

    refactored to use partial functions

--- src/main/scala/babysitter.scala
+++ src/main/scala/babysitter.scala
@@ -1,13 +1,26 @@
 package babysitter
 
 object Calculator{
+  val bedtime = 22
 
-  def apply ( start:Int, end:Int, bedtime:Int ) : Int = {
-    var new_end = end
-    var rate = 12
-    if (end < 5) {new_end = end + 24}
-    if (start >= bedtime){rate = 8}
-    val dur = new_end - start
-    return dur * rate
+  val afterBed: PartialFunction[Int, Int] = {
+    case hour if (hour >= bedtime) => 8
+}
+
+  val beforeBed: PartialFunction[Int, Int] = {
+    case hour if (hour < bedtime) => 12
+  }
+
+  def apply ( start:Int, end:Int) : Int = {
+    val rateForHour = afterBed orElse beforeBed
+    val adjusted_end = if (end < 5) (end + 24) else end
+
+    val all_hours = start until adjusted_end
+
+    val all_rates = all_hours.map(x => rateForHour(x))
+    println(all_hours)
+    println(all_rates)
+
+    return all_rates.sum
   }
 }
--- src/test/scala/babysitterSpec.scala
+++ src/test/scala/babysitterSpec.scala
@@ -7,23 +7,20 @@ class BabysitterSpec extends FlatSpec {
     val babysitterCalculator = Calculator
     val start = 18
     val end = 19
-    val bed_time = 22
-    assert (babysitterCalculator(start, end, bed_time) == 12)
+    assert (babysitterCalculator(start, end) == 12)
   }
 
   it should "be paid $24 for the two hours from 6 to 8" in {
     val babysitterCalculator = Calculator
     val start = 18
     val end = 20
-    val bed_time = 22
-    assert (babysitterCalculator(start, end, bed_time) == 24)
+    assert (babysitterCalculator(start, end) == 24)
   }
 
   it should "be paid $16 for the two hours from 10 to 12" in {
     val babysitterCalculator = Calculator
     val start = 22
     val end = 0
-    val bed_time = 22
-    assert (babysitterCalculator(start, end, bed_time) == 16)
+    assert (babysitterCalculator(start, end) == 16)
   }
 }
