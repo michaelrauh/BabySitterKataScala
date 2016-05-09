6b8c6eb1239ae6e78a5a88b14aaaa8e247692ec0
Michael Rauh <michaelrauh@MichaelRauh.local>
2016-05-09 15:11:35 -0400

    added guard to prevent after midnight to count on between range

--- src/main/scala/babysitter.scala
+++ src/main/scala/babysitter.scala
@@ -3,16 +3,16 @@ package babysitter
 object Calculator{
   val bedtime = 22
 
-  val afterBed: PartialFunction[Int, Int] = {
-    case hour if (hour >= bedtime) => 8
-}
-
   val beforeBed: PartialFunction[Int, Int] = {
     case hour if (hour < bedtime) => 12
   }
 
-  def apply ( start:Int, end:Int) : Int = {
-    val rateForHour = afterBed orElse beforeBed
+  val bedToMidnight: PartialFunction[Int, Int] = {
+    case hour if ((bedtime <= hour) && (hour <= 24)) => 8
+  }
+
+  def apply (start:Int, end:Int) : Int = {
+    val rateForHour = beforeBed orElse bedToMidnight
     val adjusted_end = if (end < 5) (end + 24) else end
 
     val all_hours = start until adjusted_end
