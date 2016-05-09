ff7a16cbb3c00e9ed8f0ab8951d9f541a44143a6
Michael Rauh <michaelrauh@MichaelRauh.local>
2016-05-09 16:29:07 -0400

    got rid of temp variables from tests

--- src/test/scala/babysitterSpec.scala
+++ src/test/scala/babysitterSpec.scala
@@ -6,44 +6,30 @@ class BabysitterSpec extends FlatSpec with Matchers {
   val babysitterCalculator = Calculator
 
   "A Babysitter" should "be paid $12 for the hour from 6 to 7" in {
-    val start = 18
-    val end = 19
-    babysitterCalculator(start, end) shouldBe 12
+    babysitterCalculator(18, 19) shouldBe 12
   }
 
   it should "be paid $24 for the two hours from 6 to 8" in {
-    val start = 18
-    val end = 20
-    babysitterCalculator(start, end) shouldBe 24
+    babysitterCalculator(18, 20) shouldBe 24
   }
 
   it should "be paid $16 for the two hours from 10 to 12" in {
-    val start = 22
-    val end = 24
-    babysitterCalculator(start, end) shouldBe 16
+    babysitterCalculator(22, 24) shouldBe 16
   }
 
   it should "be paid $32 for the two hours from 1 to 3" in {
-    val start = 25
-    val end = 27
-    babysitterCalculator(start, end) shouldBe 32
+    babysitterCalculator(25, 27) shouldBe 32
   }
 
   it should "be paid $104 for the nine hours from 6 to 3" in {
-    val start = 18
-    val end = 27
-    babysitterCalculator(start, end) shouldBe 104
+    babysitterCalculator(18, 27) shouldBe 104
   }
 
   it should "refuse to work before 5 and return 0" in {
-    val start = 14
-    val end = 18
-    babysitterCalculator(start, end) shouldBe 0
+    babysitterCalculator(14, 18) shouldBe 0
   }
 
   it should "refuse to work after 4 and return 0" in {
-    val start = 29
-    val end = 31
-    babysitterCalculator(start, end) shouldBe 0
+    babysitterCalculator(29, 31) shouldBe 0
   }
 }
