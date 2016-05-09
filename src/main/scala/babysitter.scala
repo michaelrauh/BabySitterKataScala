a9a29c38fb494bf5d935a26fc4c0b9f26f65cf12
Michael Rauh <michaelrauh@MichaelRauh.local>
2016-05-09 12:55:41 -0400

    initial very naive implementation for supporting after bedtime

--- src/main/scala/babysitter.scala
+++ src/main/scala/babysitter.scala
@@ -3,7 +3,11 @@ package babysitter
 object Calculator{
 
   def apply ( start:Int, end:Int, bedtime:Int ) : Int = {
-    val dur = end - start
-    return dur * 12
-}
+    var new_end = end
+    var rate = 12
+    if (end < 5) {new_end = end + 24}
+    if (start >= bedtime){rate = 8}
+    val dur = new_end - start
+    return dur * rate
+  }
 }
