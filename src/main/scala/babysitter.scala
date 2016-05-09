b764116e791a47e04ecba81609694b7371d3631f
Michael Rauh <michaelrauh@MichaelRauh.local>
2016-05-09 16:32:06 -0400

    allowing user to supply bedtime. This required moving function definitions into apply function scope

--- src/main/scala/babysitter.scala
+++ src/main/scala/babysitter.scala
@@ -1,21 +1,21 @@
 package babysitter
 
 object Calculator{
-  val bedtime = 22
 
-  val beforeBed: PartialFunction[Int, Int] = {
-    case hour if (hour < bedtime) => 12
-  }
+  def apply (start:Int, end:Int, bedtime:Int) : Int = {
 
-  val bedToMidnight: PartialFunction[Int, Int] = {
-    case hour if ((bedtime <= hour) && (hour <= 24)) => 8
-  }
+    val beforeBed: PartialFunction[Int, Int] = {
+      case hour if (hour < bedtime) => 12
+    }
 
-  val afterMidnight: PartialFunction[Int, Int] = {
-    case hour if (hour > 24) => 16
-  }
+    val bedToMidnight: PartialFunction[Int, Int] = {
+      case hour if ((bedtime <= hour) && (hour <= 24)) => 8
+    }
 
-  def apply (start:Int, end:Int) : Int = {
+    val afterMidnight: PartialFunction[Int, Int] = {
+      case hour if (hour > 24) => 16
+    }
+
     if (start < 17 || end > 28) {return 0}
     val rateForHour = beforeBed orElse bedToMidnight orElse afterMidnight
     return (start until end).map(x => rateForHour(x)).sum
--- src/test/scala/babysitterSpec.scala
+++ src/test/scala/babysitterSpec.scala
@@ -6,30 +6,30 @@ class BabysitterSpec extends FlatSpec with Matchers {
   val babysitterCalculator = Calculator
 
   "A Babysitter" should "be paid $12 for the hour from 6 to 7" in {
-    babysitterCalculator(18, 19) shouldBe 12
+    babysitterCalculator(18, 19, 22) shouldBe 12
   }
 
   it should "be paid $24 for the two hours from 6 to 8" in {
-    babysitterCalculator(18, 20) shouldBe 24
+    babysitterCalculator(18, 20, 22) shouldBe 24
   }
 
   it should "be paid $16 for the two hours from 10 to 12" in {
-    babysitterCalculator(22, 24) shouldBe 16
+    babysitterCalculator(22, 24, 22) shouldBe 16
   }
 
   it should "be paid $32 for the two hours from 1 to 3" in {
-    babysitterCalculator(25, 27) shouldBe 32
+    babysitterCalculator(25, 27, 22) shouldBe 32
   }
 
   it should "be paid $104 for the nine hours from 6 to 3" in {
-    babysitterCalculator(18, 27) shouldBe 104
+    babysitterCalculator(18, 27, 22) shouldBe 104
   }
 
   it should "refuse to work before 5 and return 0" in {
-    babysitterCalculator(14, 18) shouldBe 0
+    babysitterCalculator(14, 18, 22) shouldBe 0
   }
 
   it should "refuse to work after 4 and return 0" in {
-    babysitterCalculator(29, 31) shouldBe 0
+    babysitterCalculator(29, 31, 22) shouldBe 0
   }
 }
