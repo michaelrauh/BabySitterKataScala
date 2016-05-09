e0c4c6a14e2806f2af560a527ba83159a3b96ae7
Michael Rauh <michaelrauh@MichaelRauh.local>
2016-05-09 10:50:12 -0400

    first passing assert for one hour is 12

--- src/test/scala/babysitterSpec.scala
+++ src/test/scala/babysitterSpec.scala
@@ -4,10 +4,11 @@ import com.github.nscala_time.time.Imports._
 
 class BabysitterSpec extends FlatSpec {
 
-  "A Babysitter" should "be paid $12 per hour from start time to bedtime" in {
+  "A Babysitter" should "be paid $12 for the hour from 6 to 7" in {
     val babysitterCalculator = Calculator
     val start = DateTime.now.hour(18)
     val end = DateTime.now.hour(19)
-    assert (babysitterCalculator(start, end) == 12)
+    val bed_time = DateTime.now.hour(22)
+    assert (babysitterCalculator(start, end, bed_time) == 12)
   }
 }
